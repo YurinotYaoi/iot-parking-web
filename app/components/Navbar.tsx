a0b80fe4ac9be6fdd7d14e7b05dd0bcebb85487c
Angelo Uriel Solamo <yuri08182004@gmail.com>
2026-03-11 17:13:18 +0800

    Installed Next Themes and configured Initial page.tsx to route to login and signup

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { ParkingSign } from "./svg";
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-0 w-full bg-gray-800 dark:bg-gray-900 py-5 px-24 border-b border-gray-700">
+    <nav className="fixed top-0 w-full bg-gray-900 py-5 px-24 border-b border-gray-700">
       <ul className="flex items-center justify-between">
         <li className="flex items-center">
           <Link href="/" className="text-2xl font-bold text-white">
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,6 +9,24 @@ export default function Home() {
             Welcome to your{" "}
             <span className="text-zinc-300">IoT</span> frontend
           </h1>
+          <div>
+            <Link
+              href="/auth/login"
+              className="inline-block rounded-lg px-4 py-2 font-medium text-black bg-white mr-1
+              hover:bg-gray-500 hover:text-white
+              transition duration-300 ease-in-out"
+            >
+              Login
+            </Link>
+            <Link
+              href="/auth/login"
+              className="inline-block rounded-lg px-4 py-2 font-medium text-black bg-white
+              hover:bg-gray-500 hover:text-white
+              transition duration-300 ease-in-out"
+            >
+              Register
+            </Link>
+          </div>
         </div>
       </main>
     </div>
