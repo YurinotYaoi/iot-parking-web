6322d10fa7f073543efb2b33d56af310f1b93576
Angelo Uriel Solamo <yuri08182004@gmail.com>
2026-03-16 18:32:19 +0800

    Minor edits to login and signup pages, added functionality of routing to dashboard

--- app/auth/components/LoginForm.tsx
+++ app/auth/components/LoginForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { link } from "fs";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
@@ -39,18 +38,15 @@ const LoginForm = () => {
         className="block w-full rounded-md border-2 border-gray-500 p-2 text-sm text-gray-900 focus:border-gray-600"
       />
       <button
-        type="submit"
-        className="w-full rounded-md bg-blue-500 py-2 px-4 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 mt-4">
+        type="button"
+        className="w-full rounded-md bg-blue-500 py-2 px-4 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 mt-4"
+        onClick={handleNavigation}
+      >
         Login
       </button>
 
-      <button type="button" className="mt-2 bg-green-800 rounded-3xl" onClick={handleNavigation}>
-        Go To Dashboard Page
-      </button>
     </form>
   );
 };
-
-//Delete the Dashboard Button before pushing to production, this is just for testing the navigation from login to dashboard page without authentication for now
 
 export default LoginForm;
--- app/auth/components/SignUpForm.tsx
+++ app/auth/components/SignUpForm.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const SignUpForm = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    router.push('/dashboard');
+  };
 
   return (
     <form className="flex flex-col">
@@ -54,8 +61,9 @@ const SignUpForm = () => {
       />
 
       <button
-        type="submit"
-        className="w-full rounded-md bg-blue-500 py-2 px-4 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 mt-4"
+        type="button"
+        className="w-full rounded-md bg-blue-500 py-2 px-4 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 mt-4" 
+        onClick={handleNavigation}
       >
         Sign Up
       </button>
