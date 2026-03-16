28999315ae7e0a55a60f6b451547ec8a63bfc2ed
Angelo Uriel Solamo <yuri08182004@gmail.com>
2026-03-16 20:20:22 +0800

    created the dashboard layout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,21 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function DashboardScreen() {
   return (
     <main className="flex flex-col min-h-screen w-full items-center justify-center py-32 px-16
     bg-white dark:bg-black">
-      <div className="border-2 border-gray-500 p-5 rounded-2xl
-      flex flex-col items-center justify-center">
-        <h1 className="text-3xl font-semibold
-        text-black dark:text-zinc-50">
-          Dashboard Page
-        </h1>
+      <div className="min-w-screen flex flex-row w-full h-96 bg-gray-300">
+        <div className="w-[30%] p-3 bg-amber-300">
+          left
+        </div>
+        <div className="w-[70%] p-3 bg-amber-800">
+          <div className="w-full bg-amber-500">
+            <Button>Create Sensor</Button>
+            <Button>Layouts</Button>
+          </div>
+          right
+        </div>
       </div>
     </main>
   );
