da3bc8af69b148775995fdd5fc564753f8591a04
Angelo Uriel Solamo <yuri08182004@gmail.com>
2026-05-07 14:00:50 +0800

    Bug fix (Accidental removal of toast component)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable}`}> 
       <body className="antialiased overflow-x-hidden">
         <ThemeProvider attribute="class" enableSystem defaultTheme="system">
+          <Toaster />
           <Navbar />
           {children}
         </ThemeProvider>
